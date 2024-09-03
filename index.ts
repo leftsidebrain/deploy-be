8cb9f097d12557623e1f5f3b2f20c36a1d4edffe
leftsidebrain <ruddikhai10@gmail.com>
2024-09-03 10:50:20 +0700

    u

--- index.ts
+++ index.ts
@@ -11,11 +11,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: "https://deploy-2ik6xxpbp-leftsidebrains-projects.vercel.app",
-  })
-);
+app.use(cors());
 
 app.get("/", async (req, res) => {
   res.send("hello world");
